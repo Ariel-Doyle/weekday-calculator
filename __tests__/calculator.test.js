b8cd65954694b1098a71f644018e562f0eef9a42
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-18 12:53:56 -0800

    commit a test using liveshare.
    Co-authored-by: Ryan Krueger <RKrueg@users.noreply.github.com>

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -4,5 +4,5 @@ describe('Day', () => {
   test('should return a day of the week that correlates to the index position of the day', () => {
     Day.findDay('December 18, 2022');
     expect(Day.findDay()).toEqual('Sunday');
-  })
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
-/*function findDay(x) {
-  let date = new Date(x);
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const dayObject = date.getDay();
-  if (date.getDay() + 1) {
-    return days[date.getDay()];
-  } else {
-    return "This is not a valid date.";
-  }
-}*/
-
-export class Day {
+export default class Day {
   constructor() {
   }
 
-  static days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  static DaysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  function findDay(x) {
-    /*let newDate = new Date(x);
+  findDay(x) {
+    let newDate = new Date(x);
     const dayInt = newDate.getDay();
     if (dayInt + 1) {
-      return days[dayInt];
+      return daysOfTheWeek[dayInt];
     } else {
       return "This is not a valid date.";
-    }*/
+    }
   }
 }
